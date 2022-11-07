b16242e89316e2f3a17344ecc421f0e6dad1b6ab
HoangNguyen2711 <hgnguyen1999@gmail.com>
2022-11-07 08:17:57 +0700

    some custom actions

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,55 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from bs4 import BeautifulSoup
+
+import requests
+import mariadb
+import urllib.request 
+    
+mydb =  mariadb.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="guitarstoree"
+)
+cur = mydb.cursor()
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+class ask_thoitiet(Action):
+    def name(self) -> Text:
+        return "action_weather"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print('[%s] <- %s' % (self.name(), tracker.latest_message['text']))
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3', 'Accept-Language': 'vi'}
+        city= 'cantho'
+        city = city+'+weather'
+        res = requests.get(f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8', headers=headers)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        location = soup.select('#wob_loc')[0].getText().strip()
+        time = soup.select('#wob_dts')[0].getText().strip()
+        info = soup.select('#wob_dc')[0].getText().strip()
+        weather = soup.select('#wob_tm')[0].getText().strip()
+        dispatcher.utter_message(location)
+        dispatcher.utter_message(info + ' at: '+ time.lower())
+        dispatcher.utter_message('Average temperature: '+weather+"°C")
+
